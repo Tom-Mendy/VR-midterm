af02a5aee22bf75739e5d08331cab035f5319184
Tom Mendy <tom.mendy@epitech.eu>
2026-04-24 17:59:33 +0900

    Add time

---
+++ Assets/Challenges/03_Training/ActualTime.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class ActualTime : MonoBehaviour
+{
+    private TMP_Text timeText;
+
+    private void Start()
+    {
+        timeText = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        int totalSeconds = Mathf.FloorToInt(Time.time);
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        timeText.text = $"{minutes:00}:{seconds:00}";
+    }
+}
